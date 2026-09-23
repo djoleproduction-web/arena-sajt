ca90cb907d060f9a35034de6ddfc30d2e9fed073
djoleproduction-web <djoleproduction@gmail.com>
2026-09-24 00:30:05 +0200

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-// Koristimo fallback samo da Next.js ne pukne tokom bildovanja stranica
 const connectionString = process.env.DATABASE_URL || "postgresql://postgres:postgres@127.0.0.1:5432/db";
 
-const client = postgres(connectionString, { prepare: false });
+const client = postgres(connectionString, { 
+  prepare: false,
+  ssl: { rejectUnauthorized: false } 
+});
+
 export const db = drizzle(client);
